8e2905032c576a138fc10087e3a34a57297b7f3b
github-action-benchmark <github@users.noreply.github.com>
2025-06-13 22:18:02 +0000

    add test benchmark (cargo) benchmark result for e8dd453fdc93c5c898b32f7ee7e882b8ba4c402a

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1749853082889,
+  "repoUrl": "https://github.com/brycemines/testing-things",
+  "entries": {
+    "test benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "e8dd453fdc93c5c898b32f7ee7e882b8ba4c402a",
+          "message": "delete the example code comment pinging the developeer\n Please enter the commit message for your changes. Lines starting",
+          "timestamp": "2025-06-13T16:12:41-06:00",
+          "tree_id": "dccf9b6fba30104f8d63b6cf10cfedad927b198e",
+          "url": "https://github.com/brycemines/testing-things/commit/e8dd453fdc93c5c898b32f7ee7e882b8ba4c402a"
+        },
+        "date": 1749853082651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
