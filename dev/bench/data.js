0e3a2abdb533188fb3d3365d12511ed7a62190d5
brycemines <bryceirving@mines.edu>
2025-07-22 11:53:58 -0600

    initial commit containing previous benchmerk data

---
+++ dev/bench/data.js
@@ -0,0 +1,434 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1750957551019,
+  "repoUrl": "https://github.com/brycemines/testing-things",
+  "entries": {
+    "test benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "e8dd453fdc93c5c898b32f7ee7e882b8ba4c402a",
+          "message": "delete the example code comment pinging the developeer\n Please enter the commit message for your changes. Lines starting",
+          "timestamp": "2025-06-13T16:12:41-06:00",
+          "tree_id": "dccf9b6fba30104f8d63b6cf10cfedad927b198e",
+          "url": "https://github.com/brycemines/testing-things/commit/e8dd453fdc93c5c898b32f7ee7e882b8ba4c402a"
+        },
+        "date": 1749853082651,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 20",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "245b9ea284ea383cddc56f468b6c9bb5ef9b0e48",
+          "message": "add another bench to test",
+          "timestamp": "2025-06-13T16:26:29-06:00",
+          "tree_id": "232e2717bfa5d780e5a6a1eec94477fb1151d1ae",
+          "url": "https://github.com/brycemines/testing-things/commit/245b9ea284ea383cddc56f468b6c9bb5ef9b0e48"
+        },
+        "date": 1749853742286,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 6,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "id": "497a0770efb5c2a70d18c681ccb9484d35eb3f20",
+          "message": "made fib run worse",
+          "timestamp": "2025-06-13T22:27:20Z",
+          "url": "https://github.com/brycemines/testing-things/pull/1/commits/497a0770efb5c2a70d18c681ccb9484d35eb3f20"
+        },
+        "date": 1749854313174,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180136979,
+            "range": "± 1628692",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11899,
+            "range": "± 28",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "126987327+brycemines@users.noreply.github.com",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80dfaed477d7494b6cbc4da8a3fee1f3098a96e6",
+          "message": "Merge pull request #1 from brycemines/performance-hit\n\nmade fib run worse",
+          "timestamp": "2025-06-13T16:56:42-06:00",
+          "tree_id": "a94c655b4bbd384490db8bbac47bc2ef1d8f82d9",
+          "url": "https://github.com/brycemines/testing-things/commit/80dfaed477d7494b6cbc4da8a3fee1f3098a96e6"
+        },
+        "date": 1749855513619,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 216721157,
+            "range": "± 9163320",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11901,
+            "range": "± 111",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "id": "8d2ba1b4c6ff0ee964f9a741254f1d04401a6284",
+          "message": "slightly better fib",
+          "timestamp": "2025-06-13T22:56:46Z",
+          "url": "https://github.com/brycemines/testing-things/pull/2/commits/8d2ba1b4c6ff0ee964f9a741254f1d04401a6284"
+        },
+        "date": 1749856337058,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 11,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "126987327+brycemines@users.noreply.github.com",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "370749f442ce9374b4b47b19fe3fda42adcbce9b",
+          "message": "Merge pull request #2 from brycemines/performance-hit\n\nslightly better fib",
+          "timestamp": "2025-06-18T10:34:22-06:00",
+          "tree_id": "47a673ed82e7292670a6aa19683a8da649ecd03a",
+          "url": "https://github.com/brycemines/testing-things/commit/370749f442ce9374b4b47b19fe3fda42adcbce9b"
+        },
+        "date": 1750264561432,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 11,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 10,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "86b90d2044b5213c391cb9d50dfd52de19c812bf",
+          "message": "another bad performance change",
+          "timestamp": "2025-06-18T10:37:10-06:00",
+          "tree_id": "561c09ebfaecddf7da49f896010d72a25cbc11bc",
+          "url": "https://github.com/brycemines/testing-things/commit/86b90d2044b5213c391cb9d50dfd52de19c812bf"
+        },
+        "date": 1750264761557,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180236960,
+            "range": "± 898020",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11901,
+            "range": "± 39",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "197748cf7a8998cf8ede00120298d4c0b1eb4063",
+          "message": "delete target folder",
+          "timestamp": "2025-06-25T09:03:32-06:00",
+          "tree_id": "17a520a51e70ddf18e3f942c42177a4edbefc590",
+          "url": "https://github.com/brycemines/testing-things/commit/197748cf7a8998cf8ede00120298d4c0b1eb4063"
+        },
+        "date": 1750863936221,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180192408,
+            "range": "± 1456805",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11903,
+            "range": "± 84",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "d810d77ada1a342b9eb15087025e32f7eedcee67",
+          "message": "look inside the output.txt file",
+          "timestamp": "2025-06-25T10:10:45-06:00",
+          "tree_id": "336a0232df45ba635b485586e5e57c7df9615606",
+          "url": "https://github.com/brycemines/testing-things/commit/d810d77ada1a342b9eb15087025e32f7eedcee67"
+        },
+        "date": 1750867978897,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180174124,
+            "range": "± 813932",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11899,
+            "range": "± 124",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "63ebce58bca95a63d8ee3a3edcefdffa79e63f04",
+          "message": "testing a progress bar thing",
+          "timestamp": "2025-06-25T10:49:37-06:00",
+          "tree_id": "496cf09c671c7e8b6d1520419ad7446af4ee08c6",
+          "url": "https://github.com/brycemines/testing-things/commit/63ebce58bca95a63d8ee3a3edcefdffa79e63f04"
+        },
+        "date": 1750870317141,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180250281,
+            "range": "± 9350993",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11900,
+            "range": "± 243",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "25f17e6dc045ec1807568beaca8b3a2c3eb389ed",
+          "message": "modify a little bit progress bar test",
+          "timestamp": "2025-06-25T11:06:00-06:00",
+          "tree_id": "90e1b1b6754f2259cc04a4eefab0dc2649d809af",
+          "url": "https://github.com/brycemines/testing-things/commit/25f17e6dc045ec1807568beaca8b3a2c3eb389ed"
+        },
+        "date": 1750871314489,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180163373,
+            "range": "± 1715878",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11896,
+            "range": "± 32",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "committer": {
+            "email": "bryceirving@mines.edu",
+            "name": "brycemines",
+            "username": "brycemines"
+          },
+          "distinct": true,
+          "id": "772b9f1bf316f795bab3dd2f991a8daa64602ea6",
+          "message": "found a fix finally",
+          "timestamp": "2025-06-26T11:03:49-06:00",
+          "tree_id": "31de48b7614d4aebad357c797ab2beae59a0a688",
+          "url": "https://github.com/brycemines/testing-things/commit/772b9f1bf316f795bab3dd2f991a8daa64602ea6"
+        },
+        "date": 1750957550499,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib 40",
+            "value": 180268753,
+            "range": "± 3948209",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib 20",
+            "value": 11901,
+            "range": "± 37",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "kdam progress bar",
+            "value": 13676,
+            "range": "± 1254",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
