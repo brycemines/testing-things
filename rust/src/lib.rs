b97cb3d8b7894f31ecf52d46784ba179022e0ff9
brycemines <bryceirving@mines.edu>
2025-07-22 16:07:42 -0600

    triggering the rust benchmark actions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -8,6 +8,8 @@ pub fn fib(n: i32) -> i32 {
     if n <= 2 { 1 } else { fib(n - 1) + fib(n - 2) }
 }
 
+// writing a comment to get the actions to trigger
+
 #[test]
 fn test_fib() {
     assert_eq!(fib(20), 6765);
